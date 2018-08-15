399f59de1cb09dfaa2ce35428e17b2e02afaabec
mckelveygreg <mckelveygreg@gmail.com>
2018-08-15 13:09:12 -0700

    all test pass except 8, data-yvalue date object

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ const buildGraph = (data) => {
     const height = root.clientHeight;
     const padding = 50;
     const yLabel = 25;
-    const circleR = 20;
+    //const titleHeight = 25;
+    const circleR = 7;
+    const color = d3.scaleOrdinal(d3.schemeSet1);
 
     const svg = d3.select(root)
                     .append('svg')
@@ -29,7 +31,7 @@ const buildGraph = (data) => {
     const parseTimeYear = d3.timeParse('%Y');
 
     const xTimeScaleYear = d3.scaleTime()
-                                .domain([parseTimeYear(yearMin),parseTimeYear(yearMax)])
+                                .domain([parseTimeYear(yearMin - 1),parseTimeYear(yearMax + 1)])
                                 .range([padding + yLabel, width - padding]);
     const xAxis = d3.axisBottom(xTimeScaleYear).tickFormat(d3.timeFormat('%Y'));
 
@@ -62,4 +64,83 @@ const buildGraph = (data) => {
         .attr('text-anchor', 'middle')
         .attr('transform', `translate(${padding/2}, ${-25 + height/2})rotate(-90)`)
         .text('Race Time in Minutes');
+
+    // Title
+    svg.append('text')
+        .attr('transform', `translate(${width/2}, 30)`)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '28px')
+        .property('id', 'title')
+        .text('Doping in Professional Bicycle Racing')
+    svg.append('text')
+        .attr('transform', `translate(${width/2}, 50)`)
+        .attr('text-anchor', 'middle')
+        .attr('id', 'sub-title')
+        .text('35 Fastest times up Alpe d\'Huez');
+    
+    // Tooltip
+    const tooltip = d3.select('body')
+                    .append('div')
+                    .style('position', 'absolute')
+                    .style('opacity', '0')
+                    .text('filler text')
+                    .attr('id', 'tooltip')
+                    ;
+
+    //// Dots!
+    svg.selectAll('circle')
+        .data(dataset)
+        .enter()
+        .append('circle')
+        .attr('cx', d => xTimeScaleYear(parseTimeYear(d.Year)))
+        .attr('cy', d => yScaleTime(parseTimeMinSec(d.Time)))
+        .attr('r', circleR)
+        .attr('class', 'dot')
+        .attr('data-xvalue', d => d.Year)
+        .attr('data-yvalue', d => new Date(parseTimeMinSec(d.Time).toISOString()))
+        .attr('fill', d => color(d.Doping !== ''))
+        .attr('data-legend', d => d.Doping == '' ? 'No Doping Alegations' : 'Riders with Doping Alegations')
+        .on('mouseover', (d, i) => (
+            tooltip.style('opacity', '0.8')
+                    .attr('data-year', d.Year)
+                    .style('z-index', '10')
+                    .html(
+                        `${d.Name} (${d.Nationality})</br>
+                        ${d.Year}, Time: ${d.Time}</br>
+                        ${d.Doping}`
+                    )))
+        .on('mousemove', () => (
+            tooltip.style('top', (d3.event.pageY - 5) + 'px')
+                    .style('left', (d3.event.pageX + 10) + 'px')))
+        .on('mouseout', () => tooltip.style('opacity', '0').style('z-index', '-1'));
+ 
+        // Legend -- stolen from FCC :(
+        var legend = svg.selectAll(".legend")
+        .data(color.domain())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("id", "legend")
+        .attr("transform", (d, i) =>`translate(0,${height/2 - i * 20})`);
+    
+      legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+    
+      legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text((d) => {
+            console.log(d);
+          if (d) return "Riders with doping allegations";
+          else {
+            return "No doping allegations";
+          };
+        });
+
+
+
 }
